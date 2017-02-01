da7e6d2ea16f151fa20f4f592a08c2b74dd8e61b
Mila432 <mila432@gmail.com>
2017-02-01 12:35:55 +0100

    fin offset

--- extra_recipe/offsets.c
+++ extra_recipe/offsets.c
@@ -5,6 +5,7 @@
 
 #include "offsets.h"
 
+uint64_t off0;
 uint64_t off1;
 uint64_t off2;//OSData::getMetaClass
 uint64_t off3;//OSSerializer::serialize
@@ -15,16 +16,17 @@ uint64_t procoff;
 void load_offsets() {
     struct utsname u = { 0 };
     uname(&u);
-    
+
     //printf("sysname: %s\n", u.sysname);
     //printf("nodename: %s\n", u.nodename);
     //printf("release: %s\n", u.release);
     printf("version: %s\n", u.version);
     printf("machine: %s\n", u.machine);
 
-    procoff=0x360;
-    
+    procoff = 0x360;
+
     if (strcmp(u.version, "Darwin Kernel Version 16.3.0: Tue Nov 29 21:40:08 PST 2016; root:xnu-3789.32.1~4/RELEASE_ARM64_T7000") == 0) {
+        off0 = 0xFFFFFFF006FA2C50;
         off1 = 0xFFFFFFF007004000;
         off2 = 0xFFFFFFF0074446DC;
         off3 = 0xFFFFFFF00745B0DC;
@@ -32,6 +34,7 @@ void load_offsets() {
         off5 = 0xFFFFFFF0074664F8;
     }
     else if (strcmp(u.version, "Darwin Kernel Version 16.3.0: Tue Nov 29 21:40:09 PST 2016; root:xnu-3789.32.1~4/RELEASE_ARM64_T7001") == 0) {
+        off0 = 0xFFFFFFF006F85310;
         off1 = 0xFFFFFFF007004000;
         off2 = 0xFFFFFFF007444900;
         off3 = 0xFFFFFFF00745B300;
